7d11d6592d2dfd0e9021fd4078e0c16eded6a855
Felipe Pereira <felipe.dspereira@gmail.com>
2017-09-08 13:05:29 -0400

    teste

--- main.js
+++
@@ -1,8 +0,0 @@
-angular.module('meal-keeper', ['ngRoute', 'ngAnimate', 'ngTouch', 'ui.bootstrap'])
-
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when("/", {
-                templateUrl: "views/teste.html"
-            });
-    });
